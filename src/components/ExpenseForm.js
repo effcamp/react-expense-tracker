4007396b88b7e1b7f91ddb254d34f60f5c973aa8
effcamp <fcf.camposx@gmail.com>
2018-01-19 21:14:23 -0200

    Added tests for ExpenseForm

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,40 +1,34 @@
-import React from "react";
-import moment from "moment";
-import { SingleDatePicker } from "react-dates";
-import "react-dates/lib/css/_datepicker.css";
-
-// const date = new Date();
-
-// const now = moment();
-// console.log(now.format('MMM D, YYYY'));
+import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
 
 export default class ExpenseForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      description: props.expense ? props.expense.description : "",
-      note: props.expense ? props.expense.note : "",
-      amount: props.expense ? (props.expense.amount / 100).toString() : "",
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
       createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
       calendarFocused: false,
-      error: ""
+      error: ''
     };
   }
-  onDescriptionChange = e => {
+  onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(() => ({ description }));
   };
-  onNoteChange = e => {
+  onNoteChange = (e) => {
     const note = e.target.value;
     this.setState(() => ({ note }));
   };
-  onAmountChange = e => {
+  onAmountChange = (e) => {
     const amount = e.target.value;
     if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
   };
-  onDateChange = createdAt => {
+  onDateChange = (createdAt) => {
     if (createdAt) {
       this.setState(() => ({ createdAt }));
     }
@@ -42,14 +36,14 @@ export default class ExpenseForm extends React.Component {
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }));
   };
-  onSubmit = e => {
+  onSubmit = (e) => {
     e.preventDefault();
     if (!this.state.description || !this.state.amount) {
       this.setState(() => ({
-        error: "Please provide a description and amount"
+        error: 'Please provide a description and amount'
       }));
     } else {
-      this.setState(() => ({ error: "" }));
+      this.setState(() => ({ error: '' }));
       this.props.onSubmit({
         description: this.state.description,
         amount: parseFloat(this.state.amount, 10) * 100,
