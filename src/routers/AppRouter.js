552c778f6c5b7fedd2ecfe14c1e17ff09981780a
effcamp <fcf.camposx@gmail.com>
2018-02-05 20:50:51 -0200

    Login and logout history push complete

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 
 import ExpenseDashboardPage from '../components/ExpenseDashboard';
 import AddExpensePage from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
-import HelpPage from '../components/Help';
 import NotFoundPage from '../components/NotFound';
 import Header from '../components/Header';
 import Login from '../components/Login';
 
+export const history = createHistory();
+
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <div>
       <Header />
       <Switch>
@@ -18,11 +20,10 @@ const AppRouter = () => (
         <Route path="/dash" component={ExpenseDashboardPage} />
         <Route path="/create" component={AddExpensePage} />
         <Route path="/edit/:id" component={EditExpense} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 export default AppRouter;
