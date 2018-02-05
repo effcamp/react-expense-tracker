aa7ab0cf6fc3b153fc12e9068f1ad8a99d424f0b
effcamp <fcf.camposx@gmail.com>
2018-02-05 20:02:42 -0200

    Logout completed and tests for login and logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,3 +2,5 @@ import { firebase, googleAuthProvider } from '../firebase/firebase';
 
 export const startLogin = () => () =>
   firebase.auth().signInWithRedirect(googleAuthProvider);
+
+export const startLogout = () => () => firebase.auth().signOut();
--- src/tests/components/Login.test.js
+++ src/tests/components/Login.test.js
@@ -3,6 +3,13 @@ import { shallow } from 'enzyme';
 import { Login } from '../../components/Login';
 
 test('should render Login correctly', () => {
-  const wrapper = shallow(<Login />);
+  const wrapper = shallow(<Login startLogin={() => {}} />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogin on button click', () => {
+  const startLogin = jest.fn();
+  const wrapper = shallow(<Login startLogin={startLogin} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogin).toHaveBeenCalled();
 });
