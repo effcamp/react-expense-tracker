ba355a5487b9e30821cf72287021396c3b938a62
effcamp <fcf.camposx@gmail.com>
2018-02-10 09:51:39 -0200

    Dashboard page styles started

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/dash" activeClassName="is-active">
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Create Expense
-    </NavLink>
-
-    <button onClick={startLogout}>Logout</button>
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to="/dash">
+          <h1>Expensify</h1>
+        </Link>
+        <button onClick={startLogout}>Logout</button>
+      </div>
+    </div>
   </header>
 );
 
