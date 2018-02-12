4f674bf5a4a48344d73b828568df076f780c33f1
effcamp <fcf.camposx@gmail.com>
2018-02-11 22:10:38 -0200

    More styles and refactoring

--- src/app.js
+++ src/app.js
@@ -6,12 +6,11 @@ import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
 import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
-import 'react-dates/lib/css/_datepicker.css';
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
-import { userInfo } from 'os';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 
@@ -28,7 +27,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
