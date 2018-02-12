661e73102257440ef1ca78465a551c980022576d
effcamp <fcf.camposx@gmail.com>
2018-02-11 22:01:58 -0200

    Styling and refactoring

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,15 +5,23 @@ import { removeExpense } from '../actions/expenses';
 import ExpenseListItem from './ExpenseListItem';
 
 export const ExpenseList = (props) => (
-  <div>
-    {props.expenses.length === 0 ? (
-      <p>No expenses</p>
-    ) : (
-      props.expenses.map((expense) => {
-        return <ExpenseListItem key={expense.id} {...expense} />;
-      })
-    )}
-    {}
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
+      {props.expenses.length === 0 ? (
+        <div className="list-item list-item--message">
+          <span>No expenses</span>
+        </div>
+      ) : (
+        props.expenses.map((expense) => {
+          return <ExpenseListItem key={expense.id} {...expense} />;
+        })
+      )}
+    </div>
   </div>
 );
 
